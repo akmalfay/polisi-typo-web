6f591195e4b14667e20bbf1e43a09c511951db2a
akmalfay <faizagiputra.works@gmail.com>
2026-07-06 17:25:57 +0700

    Add dynamic data in Homepage and Leaderboard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime"
+
+dayjs.extend(relativeTime);
 
 function HomePage() {
     const [selectedActivity, setSelectedActivity] = useState(null);
@@ -10,7 +14,9 @@ function HomePage() {
             typo: "Teh quick brown fox",
             realTerm: "The quick brown fox",
             description: "Blablabla",
-            status: "Verified",
+            status: "verified",
+            details: "Your report on Egi was verified and awarded 10 points for accurate typo identification.",
+            createdAt: "2026-07-01T21:00:00",
         },
         {
             id: 2,
@@ -19,7 +25,9 @@ function HomePage() {
             typo: "Teh quick brown fox",
             realTerm: "The quick brown fox",
             description: "Blablabla",
-            status: "Pending",
+            status: "pending",
+            details: "Your report on Egi was verified and awarded 10 points for accurate typo identification.",
+            createdAt: "2026-07-01T10:00:00",
         },
         {
             id: 3,
@@ -28,7 +36,9 @@ function HomePage() {
             typo: "Teh quick brown fox",
             realTerm: "The quick brown fox",
             description: "Blablabla",
-            status: "Verified",
+            status: "verified",
+            details: "Your report on Egi was verified and awarded 10 points for accurate typo identification.",
+            createdAt: "2026-07-01T10:00:00",
         },
     ];
 
@@ -56,33 +66,72 @@ function HomePage() {
 
                     {/* Cards */}
                     {activities.map((activity) => (
-                        <div className='bg-white p-6 rounded-2xl shadow-md border border-slate-300 transition hover:shadow-xl hover:border-slate-400'>
-                            <div className='flex justify-between items-start gap-8'>
-                                <div>
-                                    <p className='text-lg font-medium'>Suspect: {activity.suspect}</p>
-                                    <p className='text-sm text-slate-500 mt-1'>@Username • 2 hours ago</p>
+                        <div className='flex flex-col justify-between bg-white p-6 rounded-2xl shadow-md border border-slate-300 transition hover:shadow-xl hover:border-slate-400' key={activity.id}>
+                            <div>
+                                <div className='flex justify-between items-start'>
+                                    <div>
+                                        <p className='text-lg font-medium'>Suspect: {activity.suspect}</p>
+                                        <p className='text-sm text-slate-500 mt-1'>@Username • {dayjs().to(dayjs(activity.createdAt))}</p>
+                                    </div>
+                                    <p className={activity.status == 'verified' ? 'bg-green-200 text-green-800 px-3 py-1 rounded-2xl text-sm capitalize' : 'bg-slate-200 text-slate-800 px-3 py-1 rounded-2xl text-sm capitalize'}>{activity.status}</p>
                                 </div>
-                                <p className='bg-green-200 text-green-800 px-3 py-1 rounded-2xl text-sm'>{activity.status}</p>
-                            </div>
 
-                            <div className='mt-4 space-y-4'>
-                                <div>
-                                    <p className='text-xs text-slate-500 font-semibold'>ORIGINAL</p>
-                                    <p className='text-slate-600 line-through decoration-rose-500/50 decoration-2 de font-light'>{activity.typo}</p>
-                                </div>
-                                <div>
-                                    <p className='text-xs text-slate-500 font-semibold'>CORRECTION</p>
-                                    <p>{activity.realTerm}</p>
+                                <div className='mt-4 space-y-4'>
+                                    <div>
+                                        <p className='text-xs text-slate-500 font-semibold'>ORIGINAL</p>
+                                        <p className='text-slate-600 line-through decoration-rose-500/50 decoration-2 de font-light'>{activity.typo}</p>
+                                    </div>
+                                    <div>
+                                        <p className='text-xs text-slate-500 font-semibold'>CORRECTION</p>
+                                        <p>{activity.realTerm}</p>
+                                    </div>
                                 </div>
                             </div>
 
                             <div className='pt-4 border-t border-slate-200 mt-4 text-center'>
-                                <p className='p-2 text-slate-600 font-medium rounded-2xl cursor-pointer transition hover:bg-slate-200 hover:text-slate-900'>View Details</p>
+                                <p className='p-2 text-slate-600 font-medium rounded-2xl cursor-pointer transition hover:bg-slate-200 hover:text-slate-900' onClick={() => setSelectedActivity(activity)}>View Details</p>
                             </div>
                         </div>
                     ))}
                 </div>
             </section>
+
+            {selectedActivity && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-slate-900/50 p-4">
+                    <div className="w-full max-w-2xl rounded-3xl border border-slate-300 bg-white shadow-2xl">
+                        <div className="flex items-center justify-between border-b border-slate-200 px-6 py-4">
+                            <div>
+                                <p className="text-slate-500 text-sm">Activity detail</p>
+                                <h2 className="text-xl font-semibold text-slate-900">Report on {selectedActivity.suspect} from {selectedActivity.reporter}</h2>
+                            </div>
+                            <button
+                                type="button"
+                                onClick={() => setSelectedActivity(null)}
+                                className="rounded-full px-3 py-1 text-sm font-medium text-slate-700 transition hover:bg-slate-100"
+                            >
+                                Close
+                            </button>
+                        </div>
+                        <div className="p-6 space-y-4">
+                            <div className="flex flex-wrap gap-3 text-sm">
+                                <span className="rounded-full bg-slate-100 px-3 py-1 text-slate-700">{selectedActivity.createdAt}</span>
+                                <span className="rounded-full bg-slate-100 px-3 py-1 text-slate-700">Points: {selectedActivity.status === 'verified' ? 10 : 0}</span>
+                                <span className={selectedActivity.status === 'verified' ? "rounded-full bg-green-100 px-3 py-1 text-green-700 capitalize" : "rounded-full bg-slate-100 px-3 py-1 text-slate-700 capitalize"}>{selectedActivity.status}</span>
+                            </div>
+                            <p className="text-slate-600">{selectedActivity.details}</p>
+                            <div className="flex justify-end">
+                                <button
+                                    type="button"
+                                    onClick={() => setSelectedActivity(null)}
+                                    className="rounded-2xl bg-slate-900 px-5 py-2 text-sm font-semibold text-white transition hover:bg-slate-700"
+                                >
+                                    Got it
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </main>
     )
 }
--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,4 +1,98 @@
+import dayjs from "dayjs";
+
 function Leaderboard() {
+    const topOfficers = [
+        {
+            id: 1,
+            username: "Egi",
+            totalReports: 1205,
+            TotalPoints: 12050,
+        },
+        {
+            id: 2,
+            username: "Budi",
+            totalReports: 700,
+            TotalPoints: 7000,
+        },
+        {
+            id: 3,
+            username: "Yoga",
+            totalReports: 400,
+            TotalPoints: 4000,
+        },
+        {
+            id: 4,
+            username: "Adi",
+            totalReports: 200,
+            TotalPoints: 2000,
+        },
+        {
+            id: 5,
+            username: "Udin",
+            totalReports: 100,
+            TotalPoints: 1000,
+        },
+    ]
+
+    const topTypoUsers = [
+        {
+            id: 1,
+            username: "Egi",
+            lastReported: "2026-07-01T21:00:00",
+            typoCount: 320
+        },
+        {
+            id: 2,
+            username: "Budi",
+            lastReported: "2026-07-01T12:00:00",
+            typoCount: 120
+        },
+        {
+            id: 3,
+            username: "Adi",
+            lastReported: "2026-07-01T06:00:00",
+            typoCount: 60
+        },
+        {
+            id: 4,
+            username: "Nome",
+            lastReported: "2026-07-01T12:00:00",
+            typoCount: 30
+        },
+        {
+            id: 5,
+            username: "Yui",
+            lastReported: "2026-07-01T06:00:00",
+            typoCount: 15
+        },
+    ]
+
+    const getOfficerRankDisplay = (rank) => {
+        switch (rank) {
+            case 1:
+                return "🥇"
+            case 2:
+                return "🥈"
+            case 3:
+                return "🥉"
+            default:
+                return `#${rank}`
+        }
+    }
+
+    const getTypoKingRankDisplay = (rank) => {
+        switch (rank) {
+            case 1:
+                return "💀"
+            case 2:
+                return "⚠️"
+            case 3:
+                return "⚠️"
+            default:
+                return `#${rank}`
+        }
+    }
+
     return (
         <main className="max-w-4/5 m-auto">
             <section className="my-20 text-center">
@@ -7,7 +101,8 @@ function Leaderboard() {
             </section>
             <section>
                 <div className="grid grid-cols-2 gap-4 mt-6">
-                    {/* Cards */}
+
+                    {/* Card */}
                     <div className="bg-white shadow-md border rounded-2xl border-slate-300 transition hover:shadow-xl hover:border-slate-400">
                         <div className="p-4 bg-slate-200 rounded-t-2xl flex items-center gap-2">
                             <svg fill="#032558" className="w-8" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M22.994 28.996h-14.060c-0.553 0-1 0.448-1 1s0.447 1 1 1h14.060c0.552 0 1-0.448 1-1s-0.447-1-1-1zM31.791 3.801c-0.095-0.465-0.505-0.799-0.979-0.799h-4.895v-0.998c0-0.552-0.448-1-1-1h-17.985c-0.553 0-1 0.448-1 1v0.998h-4.743c-0.474 0-0.885 0.334-0.98 0.799-0.029 0.144-0.7 3.648 1.247 6.036 1.12 1.373 2.831 2.091 5.078 2.155 1.281 3.786 4.502 6.59 8.381 7.008v6.966h-2.993c-0.553 0-1 0.448-1 1s0.447 1 1 1h8.084c0.552 0 1-0.448 1-1s-0.448-1-1-1h-3.091v-6.965c3.886-0.41 7.114-3.213 8.398-7.003 2.323-0.034 4.085-0.758 5.231-2.162 1.947-2.388 1.276-5.891 1.247-6.036zM3.013 8.58c-0.885-1.078-0.975-2.669-0.929-3.578h3.849v3.376c0 0.54 0.050 1.068 0.123 1.587-1.365-0.127-2.389-0.588-3.042-1.385zM23.917 8.378c0 4.784-3.585 8.677-7.991 8.677-4.408 0-7.994-3.893-7.994-8.677v-5.374h15.986v5.374h-0zM28.988 8.58c-0.677 0.826-1.751 1.296-3.196 1.402 0.075-0.524 0.125-1.057 0.125-1.604v-3.376h4c0.045 0.91-0.044 2.5-0.93 3.578z"></path> </g></svg>
@@ -15,44 +110,24 @@ function Leaderboard() {
                         </div>
                         <div className="p-8 divide-y divide-slate-500/30">
                             {/* Datas */}
-                            <div className="py-4 flex items-center justify-between">
-                                <div className="flex items-center gap-4">
-                                    <span className="text-primary font-semibold">#1</span>
+                            {topOfficers.map((officer, index) => (
+                                <div className="py-4 flex items-center justify-between" key={officer.id}>
+                                    <div className="flex items-center gap-4">
+                                        <span className={index + 1 === 1 || index + 1 === 2 || index + 1 === 3 ? 'text-xl text-primary font-semibold' : 'text-primary font-semibold'}>{getOfficerRankDisplay(index + 1)}</span>
+                                        <div>
+                                            <p className="font-medium">@{officer.username}</p>
+                                            <p className="text-xs text-slate-500 mt-1">{officer.totalReports} verified reports</p>
+                                        </div>
+                                    </div>
                                     <div>
-                                        <p className="font-medium">@Username</p>
-                                        <p className="text-xs text-slate-500 mt-1">1205 verified reports</p>
+                                        <p className="text-primary">{officer.TotalPoints} <span className="text-sm text-slate-500">pts</span></p>
                                     </div>
                                 </div>
-                                <div>
-                                    <p className="text-primary">15,420 <span className="text-sm text-slate-500">pts</span></p>
-                                </div>
-                            </div>
-                            <div className="py-4 flex items-center justify-between">
-                                <div className="flex items-center gap-4">
-                                    <span className="text-primary font-semibold">#1</span>
-                                    <div>
-                                        <p className="font-medium">@Username</p>
-                                        <p className="text-xs text-slate-500 mt-1">1205 verified reports</p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <p className="text-primary">15,420 <span className="text-sm text-slate-500">pts</span></p>
-                                </div>
-                            </div>
-                            <div className="py-4 flex items-center justify-between">
-                                <div className="flex items-center gap-4">
-                                    <span className="text-primary font-semibold">#1</span>
-                                    <div>
-                                        <p className="font-medium">@Username</p>
-                                        <p className="text-xs text-slate-500 mt-1">1205 verified reports</p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <p className="text-primary">15,420 <span className="text-sm text-slate-500">pts</span></p>
-                                </div>
-                            </div>
+                            ))}
                         </div>
                     </div>
+
+                    {/* Card */}
                     <div className="bg-white shadow-md border rounded-2xl border-slate-300 transition hover:shadow-xl hover:border-slate-400">
                         <div className="p-4 bg-slate-200 rounded-t-2xl flex items-center gap-2">
                             <svg fill="#032558" className="w-8" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M12,1A11,11,0,1,0,23,12,11.013,11.013,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9.011,9.011,0,0,1,12,21ZM12,4.5A7.5,7.5,0,1,0,19.5,12,7.508,7.508,0,0,0,12,4.5Zm0,13A5.5,5.5,0,1,1,17.5,12,5.506,5.506,0,0,1,12,17.5ZM12,8a4,4,0,1,0,4,4A4,4,0,0,0,12,8Zm0,6a2,2,0,1,1,2-2A2,2,0,0,1,12,14Z"></path></g></svg>
@@ -60,42 +135,20 @@ function Leaderboard() {
                         </div>
                         <div className="p-8 divide-y divide-slate-500/30">
                             {/* Datas */}
-                            <div className="py-4 flex items-center justify-between">
-                                <div className="flex items-center gap-4">
-                                    <span className="text-primary font-semibold">#1</span>
+                            {topTypoUsers.map((user, index) => (
+                                <div className="py-4 flex items-center justify-between" key={index}>
+                                    <div className="flex items-center gap-4">
+                                        <span className={index + 1 === 1 || index + 1 === 2 || index + 1 === 3 ? 'text-xl text-primary font-semibold' : 'text-primary font-semibold'}>{getTypoKingRankDisplay(index + 1)}</span>
+                                        <div>
+                                            <p className="font-medium">{user.username}</p>
+                                            <p className="text-xs text-slate-500 mt-1">Last: {dayjs(user.lastReported).format('D MMMM YYYY')}</p>
+                                        </div>
+                                    </div>
                                     <div>
-                                        <p className="font-medium">Suspect</p>
-                                        <p className="text-xs text-slate-500 mt-1">Last: 18 June 2026</p>
+                                        <p className="text-primary">{user.typoCount} <span className="text-sm text-slate-500">typos</span></p>
                                     </div>
                                 </div>
-                                <div>
-                                    <p className="text-primary">320 <span className="text-sm text-slate-500">typos</span></p>
-                                </div>
-                            </div>
-                            <div className="py-4 flex items-center justify-between">
-                                <div className="flex items-center gap-4">
-                                    <span className="text-primary font-semibold">#1</span>
-                                    <div>
-                                        <p className="font-medium">Suspect</p>
-                                        <p className="text-xs text-slate-500 mt-1">Last: 18 June 2026</p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <p className="text-primary">320 <span className="text-sm text-slate-500">typos</span></p>
-                                </div>
-                            </div>
-                            <div className="py-4 flex items-center justify-between">
-                                <div className="flex items-center gap-4">
-                                    <span className="text-primary font-semibold">#1</span>
-                                    <div>
-                                        <p className="font-medium">Suspect</p>
-                                        <p className="text-xs text-slate-500 mt-1">Last: 18 June 2026</p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <p className="text-primary">320 <span className="text-sm text-slate-500">typos</span></p>
-                                </div>
-                            </div>
+                            ))}
                         </div>
                     </div>
                 </div>
